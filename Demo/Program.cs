0a3f311b62a2c50c58d0c3f77baa699706665e67
OmarSaad <OmarSaad@DESKTOP-2C0MESG>
2025-02-18 22:33:19 +0200

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,17 @@
+namespace Demo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello Omar Saad!");
+            app.MapGet("/Index", () => "Hello World!");
+            //app.MapControllerRoute(name: "Default", 
+            //    pattern: "Controller/action/id");
+            app.Run();
+        }
+    }
+}
